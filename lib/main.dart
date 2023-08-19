1d60d319b36e56255b49afb26b79ba24dfdcca31
Can Ersoy <can.ersoy@tum.de>
2023-08-19 23:36:41 +0300

    first draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook/views/Notebook.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Notebook(),
+    );
   }
 }
--- lib/views/Notebook.dart
+++ lib/views/Notebook.dart
@@ -10,6 +10,33 @@ class Notebook extends StatefulWidget {
 class _NotebookState extends State<Notebook> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Row(
+        children: [
+          SizedBox(
+              width: MediaQuery.of(context).size.width / 4,
+              height: MediaQuery.of(context).size.height,
+              child: Placeholder()),
+          SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                textAlignVertical: TextAlignVertical.top,
+                expands: true,
+                minLines: null,
+                maxLines: null,
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              ),
+            ),
+            width: 3 * (MediaQuery.of(context).size.width) / 4,
+            height: MediaQuery.of(context).size.height,
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
