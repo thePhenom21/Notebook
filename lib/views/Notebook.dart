d060a808577c881ba9ec6adb3792c0846628f38d
Can Ersoy <can.ersoy@tum.de>
2023-08-20 17:23:01 +0300

    first http requests

--- lib/views/Notebook.dart
+++ lib/views/Notebook.dart
@@ -19,25 +19,68 @@ class _NotebookState extends State<Notebook> {
               width: MediaQuery.of(context).size.width / 4,
               height: MediaQuery.of(context).size.height,
               child: Placeholder()),
-          SizedBox(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                textAlignVertical: TextAlignVertical.top,
-                expands: true,
-                minLines: null,
-                maxLines: null,
-                decoration: InputDecoration(border: OutlineInputBorder()),
+          Column(
+            children: [
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 5.0, left: 5.0, right: 5.0, bottom: 10),
+                  child: TextField(
+                    mouseCursor: MouseCursor.uncontrolled,
+                    textAlignVertical: TextAlignVertical.top,
+                    minLines: 1,
+                    maxLines: 1,
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                  ),
+                ),
+                width: 3 * (MediaQuery.of(context).size.width) / 4,
+                height: MediaQuery.of(context).size.height / 10,
               ),
-            ),
-            width: 3 * (MediaQuery.of(context).size.width) / 4,
-            height: MediaQuery.of(context).size.height,
+              SizedBox(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 5.0, right: 5.0, bottom: 5.0),
+                  child: TextField(
+                    mouseCursor: MouseCursor.uncontrolled,
+                    textAlignVertical: TextAlignVertical.top,
+                    expands: true,
+                    minLines: null,
+                    maxLines: null,
+                    decoration: InputDecoration(border: OutlineInputBorder()),
+                  ),
+                ),
+                width: 3 * (MediaQuery.of(context).size.width) / 4,
+                height: 9 * MediaQuery.of(context).size.height / 10,
+              ),
+            ],
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton.small(
-        onPressed: () {},
-        child: Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: FloatingActionButton.small(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: FloatingActionButton.small(
+              onPressed: () {},
+              child: Icon(Icons.remove),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: FloatingActionButton.small(
+              onPressed: () {},
+              child: Icon(Icons.save),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -45,5 +88,11 @@ class _NotebookState extends State<Notebook> {
 
 createNote(String id, String title, String text, String userId) async {
   Note note = Note(id, text, title, userId);
-  Dio().post("localhost:8080/createNote/$id/$userId/$title", data: text);
+  await Dio().post("localhost:8080/createNote/$id/$userId/$title", data: text);
+}
+
+Future<List<Note>> getNotes(String userId) async {
+  return await Dio()
+      .get("localhost:8080/notes/$userId")
+      .then((value) => value.data);
 }
