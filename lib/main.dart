042d1078b2059f7f461ddb7ace869fe67475eb8e
Can Ersoy <can.ersoy@tum.de>
2023-08-20 21:51:05 +0300

    login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notebook/views/LoginPage.dart';
 import 'package:notebook/views/Notebook.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Notebook(),
+      home: LoginPage(),
     );
   }
 }
--- lib/views/LoginPage.dart
+++ lib/views/LoginPage.dart
@@ -1,10 +1,45 @@
+import 'package:flutter/material.dart';
 import 'package:riverpod/riverpod.dart';
 
-final counterProvider = StateNotifierProvider<Counter, int>((ref) {
-  return Counter();
-});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
-class Counter extends StateNotifier<int> {
-  Counter() : super(0);
-  void increment() => state++;
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(decoration: InputDecoration(hintText: "Username:")),
+            TextFormField(
+              obscureText: false,
+              decoration: InputDecoration(hintText: "Password:"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child:
+                        ElevatedButton(onPressed: () {}, child: Text("Login")),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text("Register"))
+                ],
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
 }
