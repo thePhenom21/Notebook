6c79b5276b48dac46434e8b2fbae12dca7a017fd
Can Ersoy <can.ersoy@tum.de>
2023-08-24 08:04:13 +0200

    .

--- lib/views/LoginPage.dart
+++ lib/views/LoginPage.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:binary/binary.dart';
--- lib/views/Notebook.dart
+++ lib/views/Notebook.dart
@@ -1,4 +1,3 @@
-import 'package:binary/binary.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:notebook/model/Note.dart';
